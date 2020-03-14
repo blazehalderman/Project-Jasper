f1eac6cdbb67fe49977056d06590cb17275719c4
Blaze Halderman <blazerboy123@gmail.com>
2020-03-14 16:37:13 -0700

    testing classes

--- battleship_ai/models.py
+++ battleship_ai/models.py
@@ -1,1 +1,7 @@
 # models for game pieces, players, ships, board(s)
+class PlayerClass:
+    def __init__(self, name):
+        self.name = name
+
+    def PlayerFunction(self):
+        return self.name
